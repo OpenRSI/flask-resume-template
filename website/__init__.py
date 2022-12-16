937c996c4d527a58534ef40a41e7d8003d00c1e2
ddordain <ddordain@student.42.fr>
2022-12-16 18:24:42 +0100

    upload

---
+++ website/__init__.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+
+    return app 
